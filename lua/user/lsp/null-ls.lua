09dd40fe5e5472bd8354948016df9219b15d9791
nafidhe21 <nafi.dhe21@gmail.com>
2022-09-19 20:01:34 +0700

    add format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -22,4 +22,10 @@ null_ls.setup {
     diagnostics.flake8,
     diagnostics.eslint_d,
   },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    end
+  end
+
 }
